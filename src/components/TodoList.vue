416b4684c4cabe21df3e73713b7188a791839090
Jessica Grinberg <jessgrinberg@gmail.com>
2017-03-09 12:27:44 -0800

    added component data

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -11,7 +11,30 @@
 <script type="text/javascript">
 
 export default {
-
+  props: ['todos'],
+  components: {
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false
+      }]
+    }
+  }
 }
 </script>
 
